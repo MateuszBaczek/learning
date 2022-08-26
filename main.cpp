7c79b8eae63e7ed3261ba6019a95b69a9cf2b931
Mateusz <mbaczek136@gmail.com>
2022-08-26 19:08:36 +0200

    main.exe

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main() 
 {
-    cout << "Hello me";
+    cout << "Imie- Mateusz" << endl;
+    cout << "Narodowosc- Polska" << endl;
+    cout << "Wiek- 14" << endl;
+    cout << "Hobby- brak" << endl;
+    cout << "Wykształcenie-  Szkoła podstawowa" << endl;
     return 0;
 }
-
